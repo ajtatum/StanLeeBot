78c2161f8da8dcf1892481514ace3822a648a182
AJ Tatum <andrew.j.tatum@gmail.com>
2020-01-05 16:11:38 -0500

    Updated scopes and add try/catch to AuthorizationController

--- StanLeeBot.Web/Controllers/AuthorizationController.cs
+++ StanLeeBot.Web/Controllers/AuthorizationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -32,43 +33,51 @@ namespace StanLeeBot.Web.Controllers
         [HttpGet("~/signin-slack")]
         public async Task<IActionResult> SignInSlack()
         {
-            var clientId = _appSettings.Slack.ClientId;
-            var clientSecret = _appSettings.Slack.ClientSecret;
-            var code = Request.Query["code"];
-
-            SlackAuthRequest slackAuthRequest;
-            string responseMessage;
-
-            var requestUrl = $"https://slack.com/api/oauth.access?client_id={clientId}&client_secret={clientSecret}&code={code}";
-            var request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
-            using (var client = new HttpClient())
+            try
             {
-                var response = await client.SendAsync(request).ConfigureAwait(false);
-                var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                slackAuthRequest = JsonConvert.DeserializeObject<SlackAuthRequest>(result);
-            }
+                var clientId = _appSettings.Slack.ClientId;
+                var clientSecret = _appSettings.Slack.ClientSecret;
+                var code = Request.Query["code"];
 
-            if (slackAuthRequest != null)
-            {
-                _logger.LogInformation("New installation of StanLeeBot for {TeamName} in {Channel}", slackAuthRequest.TeamName, slackAuthRequest.IncomingWebhook.Channel);
+                SlackAuthRequest slackAuthRequest;
+                string responseMessage;
 
-                var webhookUrl = slackAuthRequest.IncomingWebhook.Url;
-
-                var sbmClient = new SbmClient(webhookUrl);
-                var message = new Message
+                var requestUrl = $"https://slack.com/api/oauth.access?client_id={clientId}&client_secret={clientSecret}&code={code}";
+                var request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
+                using (var client = new HttpClient())
                 {
-                    Text = "Hi there from StanLeeBot! Checkout what I can do by typing /stanlee help"
-                };
-                await sbmClient.SendAsync(message).ConfigureAwait(false);
+                    var response = await client.SendAsync(request).ConfigureAwait(false);
+                    var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    slackAuthRequest = JsonConvert.DeserializeObject<SlackAuthRequest>(result);
+                }
 
-                responseMessage = $"Congrats! StanLeeBot has been successfully added to {slackAuthRequest.TeamName} {slackAuthRequest.IncomingWebhook.Channel}";
+                if (slackAuthRequest != null)
+                {
+                    _logger.LogInformation("New installation of StanLeeBot for {TeamName} in {Channel}", slackAuthRequest.TeamName, slackAuthRequest.IncomingWebhook.Channel);
+
+                    var webhookUrl = slackAuthRequest.IncomingWebhook.Url;
+
+                    var sbmClient = new SbmClient(webhookUrl);
+                    var message = new Message
+                    {
+                        Text = "Hi there from StanLeeBot! Checkout what I can do by typing /stanlee help"
+                    };
+                    await sbmClient.SendAsync(message).ConfigureAwait(false);
+
+                    responseMessage = $"Congrats! StanLeeBot has been successfully added to {slackAuthRequest.TeamName} {slackAuthRequest.IncomingWebhook.Channel}";
+                    return RedirectToPage("/Index", new { message = responseMessage });
+                }
+
+                _logger.LogError("Something went wrong making a request to {RequestUrl}", requestUrl);
+
+                responseMessage = "Error: Something went wrong and we were unable to add StanLeeBot to your Slack.";
                 return RedirectToPage("/Index", new { message = responseMessage });
             }
-
-            _logger.LogError("Something went wrong making a request to {RequestUrl}", requestUrl);
-
-            responseMessage = "Error: Something went wrong and we were unable to add StanLeeBot to your Slack.";
-            return RedirectToPage("/Index", new { message = responseMessage });
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "There was an error with the signin-slack method");
+                return RedirectToPage("/Index");
+            }
         }
 
         [HttpGet("~/logout"), HttpPost("~/logout")]
