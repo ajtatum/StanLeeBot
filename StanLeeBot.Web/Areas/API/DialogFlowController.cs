8a002ae408b47457a2ff46ca74cdebb2304c2a95
AJ Tatum <andrew.j.tatum@gmail.com>
2020-01-11 22:38:12 -0500

    Debugging why Staging environment isn't working. Added further debugging.

--- StanLeeBot.Web/Areas/API/DialogFlowController.cs
+++ StanLeeBot.Web/Areas/API/DialogFlowController.cs
@@ -32,86 +32,121 @@ namespace StanLeeBot.Web.Areas.API
             _googleCustomSearch = googleCustomSearch;
         }
 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("I'm alive.");
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post()
         {
-            Request.EnableBuffering();
-            var requestBody = await Request.GetRawBodyStringAsyncWithOptions(null, null, true);
-            var headers = Request.Headers;
-            var authHeader = headers[_appSettings.DialogFlow.HeaderName].ToString();
-
             _logger.LogDebug("DialogFlow: Received Request");
 
-            if (authHeader == _appSettings.DialogFlow.HeaderValue)
+            try
             {
-                _logger.LogDebug("DialogFlow: AuthHeaders match!");
+                Request.EnableBuffering();
+                var requestBody = await Request.GetRawBodyStringAsyncWithOptions(null, null, true);
+                var headers = Request.Headers;
+                var authHeader = headers[_appSettings.DialogFlow.HeaderName].ToString();
 
-                var webhookRequest = JsonConvert.DeserializeObject<WebhookRequest>(requestBody);
-                _logger.LogInformation("DialogFlow: Received WebHookRequest: {WebHookRequest}", webhookRequest);
+                _logger.LogDebug("DialogFlow: Beginning Request header validation.");
 
-                var intentName = webhookRequest.QueryResult.Intent.Name;
-                intentName = intentName.Substring(intentName.LastIndexOf("/", StringComparison.Ordinal) + 1);
-
-                var searchFor = webhookRequest.QueryResult.QueryText;
-
-                var webHookResponse = new WebhookResponse();
-
-                switch (intentName)
+                if (authHeader == _appSettings.DialogFlow.HeaderValue)
                 {
-                    case Constants.DialogFlow.HelloMarvelLookupId:
-                        webHookResponse.FulfillmentMessages.Add(await GetMarvelCard(searchFor));
-                        break;
-                    case Constants.DialogFlow.HelloDCLookupId:
-                        webHookResponse.FulfillmentMessages.Add(await GetDcComicsCard(searchFor));
-                        break;
-                    default:
-                        webHookResponse.FulfillmentText = "Sorry, but I don't understand.";
-                        break;
+                    _logger.LogDebug("DialogFlow: Request header matches!");
+
+                    var webhookRequest = JsonConvert.DeserializeObject<WebhookRequest>(requestBody);
+                    _logger.LogInformation("DialogFlow: Received WebHookRequest: {WebHookRequest}", webhookRequest);
+
+                    var intentName = webhookRequest.QueryResult.Intent.Name;
+                    intentName = intentName.Substring(intentName.LastIndexOf("/", StringComparison.Ordinal) + 1);
+
+                    var queryText = webhookRequest.QueryResult.QueryText;
+
+                    _logger.LogInformation("DialogFlow: Processing request for intent {Intent} and queryText {QueryText}", intentName, queryText);
+
+                    var webHookResponse = new WebhookResponse();
+
+                    switch (intentName)
+                    {
+                        case Constants.DialogFlow.HelloMarvelLookupId:
+                            _logger.LogDebug("DialogFlow: Beginning to request Marvel Lookup.");
+                            webHookResponse.FulfillmentMessages.Add(await GetMarvelCard(queryText));
+                            break;
+                        case Constants.DialogFlow.HelloDCLookupId:
+                            _logger.LogDebug("DialogFlow: Beginning to request DC Comics Lookup.");
+                            webHookResponse.FulfillmentMessages.Add(await GetDcComicsCard(queryText));
+                            break;
+                        default:
+                            _logger.LogDebug("DialogFlow: Unhandled intent: {IntentName}", intentName);
+                            webHookResponse.FulfillmentText = "Sorry, but I don't understand.";
+                            break;
+                    }
+
+                    return new OkObjectResult(webHookResponse);
                 }
 
-                return new OkObjectResult(webHookResponse);
+                if (authHeader.IsNullOrWhiteSpace())
+                    _logger.LogError("DialogFlow: Request header is absent.");
+                else
+                    _logger.LogError("DialogFlow: Request header do not match. Value sent: {AuthHeader}", authHeader);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "DialogFlow: Error during request.");
+                return new BadRequestResult();
             }
 
-            if(authHeader.IsNullOrWhiteSpace())
-                _logger.LogError("DialogFlow: Request header is absent.");
-            else
-            {
-                _logger.LogError("DialogFlow: Request header do not match.");
-            }
             return new UnauthorizedResult();
         }
 
-        private async Task<RepeatedField<Intent.Types.Message>> GetMarvelCard(string lookingFor)
+        private async Task<RepeatedField<Intent.Types.Message>> GetMarvelCard(string searchTerm)
         {
-            var marvelGoogleCx = _appSettings.GoogleCustomSearch.MarvelCx;
-            var gsr = await _googleCustomSearch.GetResponse(lookingFor, marvelGoogleCx);
-
-            var gsrMetaTags = gsr.Items.ElementAtOrDefault(0)?.PageMap.MetaTags.ElementAtOrDefault(0) ?? new MetaTag();
+            _logger.LogInformation("DialogFlow: GetMarvelCard - Received request to search for {SearchTerm}", searchTerm);
 
             var backupImage = "https://stanleebot.com/images/DialogFlow/Messenger/MarvelCard.png";
 
             var marvelCard = new Intent.Types.Message.Types.Card
             {
-                Title = "Sorry", 
-                Subtitle = $"Couldn't find anything for {lookingFor}", 
+                Title = "Sorry",
+                Subtitle = $"Couldn't find anything for {searchTerm}",
                 ImageUri = backupImage
             };
 
-            if (gsr != null)
+            try
             {
-                var title = gsr.Items.ElementAtOrDefault(0)?.Title.Split("|").ElementAtOrDefault(0)?.Trim() ?? lookingFor;
-                marvelCard.Title = title;
+                var marvelGoogleCx = _appSettings.GoogleCustomSearch.MarvelCx;
+                var gsr = await _googleCustomSearch.GetResponse(searchTerm, marvelGoogleCx);
 
-                var bio = gsr.Items.ElementAtOrDefault(0)?.Snippet.CleanString() ?? string.Empty;
-                marvelCard.Subtitle = bio;
+                var gsrMetaTags = gsr?.Items.ElementAtOrDefault(0)?.PageMap.MetaTags.ElementAtOrDefault(0);
 
-                marvelCard.ImageUri = gsr.Items.ElementAtOrDefault(0)?.PageMap.CseImage.ElementAtOrDefault(0)?.Src ?? backupImage;
-                
-                marvelCard.Buttons.Add(new Intent.Types.Message.Types.Card.Types.Button()
+                if (gsrMetaTags != null)
                 {
-                    Postback = gsrMetaTags.OgUrl,
-                    Text = "Excelsior! Read more..."
-                });
+                    _logger.LogDebug("DialogFlow: GetMarvelCard - Beginning to build card for {SearchTerm}.", searchTerm);
+
+                    var title = gsr.Items.ElementAtOrDefault(0)?.Title.Split("|").ElementAtOrDefault(0)?.Trim() ?? searchTerm;
+                    marvelCard.Title = title;
+
+                    var bio = gsr.Items.ElementAtOrDefault(0)?.Snippet.CleanString() ?? string.Empty;
+                    marvelCard.Subtitle = bio;
+
+                    marvelCard.ImageUri = gsr.Items.ElementAtOrDefault(0)?.PageMap.CseImage.ElementAtOrDefault(0)?.Src ?? backupImage;
+
+                    marvelCard.Buttons.Add(new Intent.Types.Message.Types.Card.Types.Button()
+                    {
+                        Postback = gsrMetaTags.OgUrl,
+                        Text = "Excelsior! Read more..."
+                    });
+
+                    _logger.LogDebug("DialogFlow: GetMarvelCard - Card built for {SearchTerm}.", searchTerm);
+                }
+
+                _logger.LogError("DialogFlow: GetMarvelCard - Metatags are null for {SearchTerm}", searchTerm);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "DialogFlow: GetMarvelCard - Error during search request for {SearchTerm}.", searchTerm);
             }
 
             var messages = new RepeatedField<Intent.Types.Message>
@@ -125,37 +160,52 @@ namespace StanLeeBot.Web.Areas.API
             return messages;
         }
 
-        private async Task<RepeatedField<Intent.Types.Message>> GetDcComicsCard(string lookingFor)
+        private async Task<RepeatedField<Intent.Types.Message>> GetDcComicsCard(string searchTerm)
         {
-            var dcComicsCx = _appSettings.GoogleCustomSearch.DcComicsCx;
-            var gsr = await _googleCustomSearch.GetResponse(lookingFor, dcComicsCx);
-
-            var gsrMetaTags = gsr.Items.ElementAtOrDefault(0)?.PageMap.MetaTags.ElementAtOrDefault(0) ?? new MetaTag();
+            _logger.LogInformation("DialogFlow: GetDcComicsCard - Received request to search for {SearchTerm}", searchTerm);
 
             var backupImage = "https://stanleebot.com/images/DialogFlow/Messenger/DCCard.png";
 
             var dcCard = new Intent.Types.Message.Types.Card
             {
                 Title = "Sorry",
-                Subtitle = $"Couldn't find anything for {lookingFor}",
+                Subtitle = $"Couldn't find anything for {searchTerm}",
                 ImageUri = backupImage
             };
 
-            if (gsr != null)
+            try
             {
-                var title = gsrMetaTags.OgTitle ?? lookingFor;
-                dcCard.Title = title;
+                var dcComicsCx = _appSettings.GoogleCustomSearch.DcComicsCx;
+                var gsr = await _googleCustomSearch.GetResponse(searchTerm, dcComicsCx);
 
-                var bio = gsr.Items.ElementAtOrDefault(0)?.Snippet.CleanString() ?? string.Empty;
-                dcCard.Subtitle = bio;
+                var gsrMetaTags = gsr?.Items.ElementAtOrDefault(0)?.PageMap.MetaTags.ElementAtOrDefault(0);
 
-                dcCard.ImageUri = gsr.Items.ElementAtOrDefault(0)?.PageMap.CseThumbnail.ElementAtOrDefault(0)?.Src ?? backupImage;
-
-                dcCard.Buttons.Add(new Intent.Types.Message.Types.Card.Types.Button()
+                if (gsrMetaTags != null)
                 {
-                    Postback = gsrMetaTags.OgUrl,
-                    Text = "Excelsior! Read more..."
-                });
+                    _logger.LogDebug("DialogFlow: GetDcComicsCard - Beginning to build card for {SearchTerm}.", searchTerm);
+
+                    var title = gsrMetaTags.OgTitle ?? searchTerm;
+                    dcCard.Title = title;
+
+                    var bio = gsr.Items.ElementAtOrDefault(0)?.Snippet.CleanString() ?? string.Empty;
+                    dcCard.Subtitle = bio;
+
+                    dcCard.ImageUri = gsr.Items.ElementAtOrDefault(0)?.PageMap.CseThumbnail.ElementAtOrDefault(0)?.Src ?? backupImage;
+
+                    dcCard.Buttons.Add(new Intent.Types.Message.Types.Card.Types.Button()
+                    {
+                        Postback = gsrMetaTags.OgUrl,
+                        Text = "Excelsior! Read more..."
+                    });
+
+                    _logger.LogDebug("DialogFlow: GetDcComicsCard - Card built for {SearchTerm}.", searchTerm);
+                }
+
+                _logger.LogError("DialogFlow: GetDcComicsCard - Metatags are null for {SearchTerm}", searchTerm);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "DialogFlow: GetDcComicsCard - Error during search request for {SearchTerm}.", searchTerm);
             }
 
             var messages = new RepeatedField<Intent.Types.Message>
