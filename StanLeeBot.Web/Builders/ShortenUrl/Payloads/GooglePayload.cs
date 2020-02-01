cbb98ab7c927386a434b9b9a46503c779ac79b9c
AJ Tatum <andrew.j.tatum@gmail.com>
2020-02-01 02:23:05 -0500

    Added payloads for shortening urls

---
+++ StanLeeBot.Web/Builders/ShortenUrl/Payloads/GooglePayload.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using StanLeeBot.Web.Models.DialogFlow.Payloads;
+
+namespace StanLeeBot.Web.Builders.ShortenUrl.Payloads
+{
+    public class GooglePayload
+    {
+        public static GooglePayloadSettings.Payload Build(string messageText)
+        {
+            var payloadSettings = new GooglePayloadSettings.Payload()
+            {
+                ExpectUserResponse = false,
+                RichResponse = new GooglePayloadSettings.RichResponse()
+                {
+                    Items = new List<GooglePayloadSettings.Item>()
+                    {
+                        new GooglePayloadSettings.Item()
+                        {
+                            SimpleResponse = new GooglePayloadSettings.SimpleResponse()
+                            {
+                                TextToSpeech = messageText,
+                                DisplayText = messageText
+                            }
+                        }
+                    }
+                }
+            };
+
+            return payloadSettings;
+        }
+    }
+}
